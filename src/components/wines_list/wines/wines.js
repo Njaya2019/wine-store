f01ea85b454da5ad4c01aee285bc673682eb0e49
njaya <njayaandrew@gmail.com>
2021-04-13 07:13:10 +0300

    [chore] created wine containers for desktop and mobile screens

--- src/components/wines_list/wines/wines.js
+++ src/components/wines_list/wines/wines.js
@@ -4,37 +4,58 @@ import style from './wines.css'
 const Wines = (props) => {
     return (
         <div className={style.wines_container}>
-
+            {/* Wine container */}
             <div className={style.wine_container}>
+                {/* Left container */}
                 <div className={style.image_container}
                     style={{
                         background: `url(../wines/2.jpg)`
                     }}
                 >
-
                 </div>
-
+                {/* Right container */}
                 <div className={style.right_wine_container}>
+                    {/* Wine name container */}
                     <div className={style.name_container}>
                         <h1>13 Breidecker</h1>
-
                     </div>
-
+                    {/* Bottle or case container*/}
                     <div class={style.bottle_case_container}>
+                        {/* Bottle  container*/}
                         <div className={style.bottle_container}>
+                            {/* bottle price container */}
+                            <div className={style.bottle_and_price}>
+                                <div className={style.bottle}>Bottle</div>
+                                <div className={style.bottle_price}>$17.90</div>
+                            </div>
+                            {/* bottle quantity container */}
+                            <div className={style.bottle_quantity}>
+                                <input type="number"/>
+                                <span>QTY</span>
+                            </div>
 
                         </div>
+                        {/* Case container */}
                         <div className={style.case_container}>
-
+                            {/* case price container */}
+                           <div className={style.case_and_price}>
+                                <div className={style.case}>Case</div>
+                                <div className={style.case_price}>$204.06</div>
+                            </div>
+                            {/* case quantity container */}
+                            <div className={style.case_quantity}>
+                                <input type="number"/>
+                                <span>QTY</span>
+                            </div>
                         </div>
                     </div>
 
                     <div className={style.cart_details_container}>
                         <div className={style.details_container}>
-
+                            Details
                         </div>
                         <div class={style.cart_container}>
-
+                            Add to cart
                         </div>
 
                     </div>
